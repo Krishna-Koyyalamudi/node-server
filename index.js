1bccde3377e93e60f829e75f698198c477dbf7a6
Krishna-Koyyalamudi <60024842+Krishna-Koyyalamudi@users.noreply.github.com>
2020-03-24 22:47:56 -0500

    b

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-const hostname = '0.0.0.0';
-const port = 4444;
+const hostname = process.env.hostname || '0.0.0.0';
+const port = process.env.PORT || 4444;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
